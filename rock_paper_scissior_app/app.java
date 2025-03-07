2d87137e071f06c19010289bc51c3b5559c10b39
Hardik-chauhan077 <hardikchauhan200305@gmail.com>
2025-03-07 19:56:13 +0530

    Update app.java

--- rock_paper_scissior_app/app.java
+++ rock_paper_scissior_app/app.java
@@ -2,20 +2,12 @@ package rock_paper_scissior_app;
 
 import javax.swing.*;
 
-// backend
 public class app {
     public static void main(String[] args) {
-
-        // we creting the thred that  run the our gui
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                rock_paper_scissor_app.rockpaperscissiorgui rockpapersisgui = new rock_paper_scissor_app.rockpaperscissiorgui(); // object that run in the  thread
-
-                // display the gui
-                rockpapersisgui.setVisible(true);
-            }
+        // Create and run the GUI in a separate thread
+        SwingUtilities.invokeLater(() -> {
+            rockpaperscissiorgui rockpaperscissiorGUI = new rockpaperscissiorgui();
+            rockpaperscissiorGUI.setVisible(true);  // Display the GUI
         });
-
     }
 }
